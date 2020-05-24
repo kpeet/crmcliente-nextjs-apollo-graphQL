ed6d8c8f778ceaf469e8db823761590814824c5f
Kenneth Peet <kpeet@inf.utfsm.cl>
2020-05-24 16:33:19 -0400

    base front project

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Layout from '../components/Layout';
 
 const Nosotros = () =>{
     return (
+        <div>
+          <Layout>
         <h1>Nosotros</h1>
+          </Layout>
+        </div>
     )
 }
 
---
+++ pages/productos.js
@@ -0,0 +1,12 @@
+
+import Layout from '../components/Layout';
+
+const Index = () => (
+    <div>
+        <Layout>
+            <h1 className="text-2xl text-gray-800 font-light">Productos</h1>
+        </Layout>
+    </div>
+);
+
+export default Index;
