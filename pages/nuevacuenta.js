7809fdeaaa2fca6c95092dc6c67dacc41070a774
Kenneth Peet <kpeet@inf.utfsm.cl>
2020-05-24 17:37:24 -0400

    formulario nueva cuenta

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -1,10 +1,72 @@
-
 import Layout from '../components/Layout';
 
 const NuevaCuenta = () => (
     <div>
         <Layout>
-            <h1 className="text-2xl text-gray-800 font-light">NuevaCuenta</h1>
+            <h1 className="text-center text-2xl text-white font-light">Nueva Cuenta</h1>
+
+            <div className="flex justify-center mt-5">
+
+                <div className="w-full max-w-sm">
+                    <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
+                                Nombre
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
+                                id="nombre"
+                                type="text"
+                                placeholder="Nombre Usuario"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
+                                Apellido
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
+                                id="Apellido"
+                                type="text"
+                                placeholder="Apellido Usuario"
+                            />
+                        </div>
+                        <div  className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                Email
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
+                                id="email"
+                                type="email"
+                                placeholder="Email Usuario"
+                            />
+                        </div>
+
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                Password
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
+                                id="password"
+                                type="password"
+                                placeholder="Password Usuario"
+                            />
+                        </div>
+
+                        <input
+                            type="submit"
+                            className="bg-gray-800 w-full mt-5 pt-2 text-white uppercas hover:bg-gray-9git 00"
+                            value="Iniciar Sesión"
+                        />
+                    </form>
+
+                </div>
+
+            </div>
+
+
         </Layout>
     </div>
 );
