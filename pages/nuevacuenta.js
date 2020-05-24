58532c6a682294c6e2e8e476a101a330feb5a365
Kenneth Peet <kpeet@inf.utfsm.cl>
2020-05-24 18:05:26 -0400

    formulario nueva cuenta con Formik

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -1,74 +1,101 @@
 import Layout from '../components/Layout';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
-const NuevaCuenta = () => (
-    <div>
-        <Layout>
-            <h1 className="text-center text-2xl text-white font-light">Nueva Cuenta</h1>
+const NuevaCuenta = () => {
 
-            <div className="flex justify-center mt-5">
+    //Validacion del formulario
+    const formik = useFormik({
+        initialValues: {
+            nombre: '',
+            apellido: '',
+            email: '',
+            password: ''
+        },
+        onSubmit: valores => {
+            console.log('enviando')
+            console.log(valores)
+        }
+    });
+    return (
+        <div>
+            <Layout>
+                <h1 className="text-center text-2xl text-white font-light">Nueva Cuenta</h1>
 
-                <div className="w-full max-w-sm">
-                    <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
-                                Nombre
-                            </label>
+                <div className="flex justify-center mt-5">
+
+                    <div className="w-full max-w-sm">
+                        <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                              onSubmit={formik.handleSubmit}
+                        >
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
+                                    Nombre
+                                </label>
+                                <input
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
+                                    id="nombre"
+                                    type="text"
+                                    placeholder="Nombre Usuario"
+                                    value={formik.values.nombre}
+                                    onChange={formik.handleChange}
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
+                                    Apellido
+                                </label>
+                                <input
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
+                                    id="apellido"
+                                    type="text"
+                                    placeholder="Apellido Usuario"
+                                    value={formik.values.apellido}
+                                    onChange={formik.handleChange}
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                    Email
+                                </label>
+                                <input
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
+                                    id="email"
+                                    type="email"
+                                    placeholder="Email Usuario"
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
+                                />
+                            </div>
+
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                    Password
+                                </label>
+                                <input
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
+                                    id="password"
+                                    type="password"
+                                    placeholder="Password Usuario"
+                                    value={formik.values.password}
+                                    onChange={formik.handleChange}
+                                />
+                            </div>
+
                             <input
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
-                                id="nombre"
-                                type="text"
-                                placeholder="Nombre Usuario"
+                                type="submit"
+                                className="bg-gray-800 w-full mt-5 pt-2 text-white uppercas hover:bg-gray-9git 00"
+                                value="Iniciar Sesión"
                             />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
-                                Apellido
-                            </label>
-                            <input
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
-                                id="Apellido"
-                                type="text"
-                                placeholder="Apellido Usuario"
-                            />
-                        </div>
-                        <div  className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                                Email
-                            </label>
-                            <input
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
-                                id="email"
-                                type="email"
-                                placeholder="Email Usuario"
-                            />
-                        </div>
+                        </form>
 
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                                Password
-                            </label>
-                            <input
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
-                                id="password"
-                                type="password"
-                                placeholder="Password Usuario"
-                            />
-                        </div>
-
-                        <input
-                            type="submit"
-                            className="bg-gray-800 w-full mt-5 pt-2 text-white uppercas hover:bg-gray-9git 00"
-                            value="Iniciar Sesión"
-                        />
-                    </form>
+                    </div>
 
                 </div>
 
-            </div>
 
-
-        </Layout>
-    </div>
-);
+            </Layout>
+        </div>)
+};
 
 export default NuevaCuenta;
