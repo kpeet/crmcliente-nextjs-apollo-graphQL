c94d3197f4fd8db394bd8d3b50c97b8271d2b1d0
Kenneth Peet <kpeet@inf.utfsm.cl>
2020-05-25 23:22:37 -0400

    add jwt Bearer autentication

--- config/apollo.js
+++ config/apollo.js
@@ -1,8 +1,36 @@
-import { ApolloClient, HttpLink , InMemoryCache} from '@apollo/client';
+import { ApolloClient, HttpLink, createHttpLink , InMemoryCache} from '@apollo/client';
 import fetch from 'node-fetch';
+import { setContext } from 'apollo-link-context'
 
+
+const httpsLink = createHttpLink({
+    uri: 'http://localhost:4000',
+    fetch
+})
+
+
+const authLink = setContext((_, {headers}) => {
+
+    //Leer el storage
+    const token = localStorage.getItem('token')
+
+    return {
+        headers:{
+            ... headers,
+            autorization: token ? `Bearer ${token}` : ''
+        }
+    }
+});
 
 const client = new ApolloClient({
+    connectToDevTools: true,
+    cache: new InMemoryCache(),
+    link: authLink.concat( httpsLink )
+
+
+});
+
+const client_sin_autenticacion = new ApolloClient({
     connectToDevTools: true,
     cache: new InMemoryCache(),
     link: new HttpLink({
